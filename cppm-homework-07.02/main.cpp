47ca6e7ede85eb141bf20bcbf59a4e3f493c2bcf
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-11-24 19:38:07 +0300

    Initial Commit

---
+++ cppm-homework-07.02/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cppm-homework-07.02
+//
+//  Created by a1ex on 11/24/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
